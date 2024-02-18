7d6c40ee6e58e162ff6ae963fc9b1ef76f0aa34a
RobertLYucra <rlujany@indracompany.com>
2024-02-17 23:45:07 -0500

    Corrigiendo hasrouter

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,9 @@ import "./index.css";
 import { BrowserRouter, HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <HashRouter>
-    <React.StrictMode>
+  <React.StrictMode>
+    <HashRouter>
       <App />
-    </React.StrictMode>
-  </HashRouter>
+    </HashRouter>
+  </React.StrictMode>
 );
